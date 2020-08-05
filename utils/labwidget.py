16f6b1a0f00dc45d5a18f2f9482f5a333de4af6b
David Bau <davidbau@csail.mit.edu>
2020-08-05 15:06:58 -0400

    Add license.

--- utils/labwidget.py
+++ utils/labwidget.py
@@ -19,7 +19,28 @@ the python model and notify any registered python listeners; similarly
 model.on('propname', callback) will listen for property changes
 that come from python.
 
-TODO: Support jupyterlab also.
+MIT LICENSE
+
+Copyright 2020 David Bau
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+
 """
 
 import json
@@ -183,7 +204,7 @@ class Widget(Model):
         Override to define the javascript logic for the widget.  Should
         render the initial view based on the current model state (if not
         already rendered using widget_html) and set up listeners to keep
-        the model and the view synchornized.
+        the model and the view synchronized.
         '''
         return ''
 
