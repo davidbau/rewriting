6dbb9a8f1f6015dc68ee7801f3c05d68792e7d31
David Bau <davidbau@csail.mit.edu>
2020-10-02 18:47:47 -0400

    Use torch.hub for model downloads.

--- utils/stylegan2/__init__.py
+++ utils/stylegan2/__init__.py
@@ -9,26 +9,31 @@
 import os
 
 import torch
-from torch.utils import model_zoo
 
 from .models import SeqStyleGAN2
 from collections import defaultdict
 
 # TODO: change these paths to non-antonio paths, probably load from url if not exists
-WEIGHT_URLS = 'http://wednesday.csail.mit.edu/placesgan/tracer/utils/stylegan2/weights/'
+WEIGHT_URLS = 'http://rewriting.csail.mit.edu/data/models/'
 sizes = defaultdict(lambda: 256, faces=1024, car=512)
 
-def load_state_dict(category):
-    chkpt_name = f'stylegan2_{category}.pt'
-    model_path = os.path.join('weights', chkpt_name)
-    os.makedirs('weights', exist_ok=True)
+FILENAMES = dict(
+    bedroom='stylegan2_bedroom-6fa55a6e.pt',
+    car='stylegan2_car-3659b4b6.pt',
+    cat='stylegan2_cat-d8dc98b2.pt',
+    church='stylegan2_church-e8ca9fd0.pt',
+    faces='stylegan2_faces-2858cc2e.pt',
+    horse='stylegan2_horse-499b5380.pt',
+    kitchen='stylegan2_kitchen-b3a526e9.pt',
+    places='stylegan2_places-a3b72d71.pt'
+)
 
-    if not os.path.exists(model_path):
-        url = WEIGHT_URLS + chkpt_name
-        state_dict = model_zoo.load_url(url, model_dir='weights', progress=True)
-        torch.save(state_dict, model_path)
-    else:
-        state_dict = torch.load(model_path)
+def load_state_dict(category):
+    url = WEIGHT_URLS + FILENAMES[category]
+    try:
+        sd = torch.hub.load_state_dict_from_url(url)  # pytorch 1.1
+    except:
+        sd = torch.hub.model_zoo.load_url(url)  # pytorch 1.0
     return state_dict
     
 def load_seq_stylegan(category, truncation=1.0, **kwargs):  # mconv='seq'):
